366c37123a071b062a7dad1cdc42c7e9686b84cc
Fumiya Mitsuji <a5820079@aoyama.jp>
2024-02-19 20:34:19 +0900

    コード修正

--- pipeline/data_reader.py
+++ pipeline/data_reader.py
@@ -1,43 +1,49 @@
 import json
 
 
-def read_json_file(file_path):
-    with open(file_path, "r", encoding="UTF-8") as input_file:
-        data = json.load(input_file)
+def read_dataset_file(dataset, file_path):
+    if dataset == "lcquad2" or "webqsp":
+        with open(file_path, "r", encoding="UTF-8") as input_file:
+            data = json.load(input_file)
+        return data
+    elif dataset == "simpleqs":
+        with open(file_path, "r", encoding="UTF-8") as input_file:
+            data = list(input_file)
         return data
 
 
-def get_prompt(dataset, input):
+def get_few_shots(dataset):
     if dataset == 'lcquad2':
         return f'''INPUT: What does emigration mean?
-                OUTPUT: \"entities_text\": [], \"wikipedia_urls\": []
-                INPUT: Who is the child of Ranavalona I's husband?
-                OUTPUT: \"entities_text\": [\"Ranavalona I\"], \"wikipedia_urls\": [\"https://en.wikipedia.org/wiki/Ranavalona_I\"]
-                INPUT: What periodical literature does Delta Air Lines use as a moutpiece?
-                OUTPUT: \"entities_text\": [\"periodical literature\", \"Delta Air Lines\"], \"wikipedia_urls\": [\"https://en.wikipedia.org/wiki/Periodical_literature\", \"https://en.wikipedia.org/wiki/Delta_Air_Lines\"]
-                INPUT: What is award received of Hans Krebs where point in time is 1966-0-0?
-                OUTPUT: \"entities_text\": [\"Hans Krebs\"], \"wikipedia_urls\": [\"https://en.wikipedia.org/wiki/Hans_Krebs_(biochemist)\"]
-                INPUT: {input}
-                OUTPUT:'''
+OUTPUT: "entities_text": [], "wikipedia_urls": []
+INPUT: Who is the child of Ranavalona I's husband?
+OUTPUT: "entities_text": ["Ranavalona I"], "wikipedia_urls": ["https://en.wikipedia.org/wiki/Ranavalona_I"]
+INPUT: What periodical literature does Delta Air Lines use as a moutpiece?
+OUTPUT: "entities_text": ["periodical literature", "Delta Air Lines"], "wikipedia_urls": ["https://en.wikipedia.org/wiki/Periodical_literature", "https://en.wikipedia.org/wiki/Delta_Air_Lines"]
+INPUT: What is award received of Hans Krebs where point in time is 1966-0-0?
+OUTPUT: "entities_text": ["Hans Krebs"], "wikipedia_urls": ["https://en.wikipedia.org/wiki/Hans_Krebs_(biochemist)"]'''
 
     elif dataset == 'simpleqs':
         return f'''INPUT: How do plants grow?
-                OUTPUT: \"entities_text\": [], \"wikipedia_urls\": []
-                INPUT: what movie is produced by warner bros.
-                OUTPUT: \"entities_text\": [\"warner bros.\"], \"wikipedia_urls\": [\"https://en.wikipedia.org/wiki/Warner_Bros.\"]
-                INPUT: what is the gender of james hendry?
-                OUTPUT: \"entities_text\": [\"james hendry\"], \"wikipedia_urls\": [\"https://en.wikipedia.org/wiki/James_Hendry_(obstetrician)\"]
-                INPUT: {input}
-                OUTPUT:'''
+OUTPUT: "entities_text": [], "wikipedia_urls": []
+INPUT: what movie is produced by warner bros.
+OUTPUT: "entities_text": ["warner bros."], "wikipedia_urls": ["https://en.wikipedia.org/wiki/Warner_Bros."]
+INPUT: what is the gender of james hendry?
+OUTPUT: "entities_text": ["james hendry"], "wikipedia_urls": ["https://en.wikipedia.org/wiki/James_Hendry_(obstetrician)"]'''
 
     elif dataset == 'webqsp':
-        return f'''INPUT: what does the letters eu stand for?
-                OUTPUT: \"entities_text\": [], \"wikipedia_urls\": []
-                INPUT: what country is the grand bahama island in?
-                OUTPUT: \"entities_text\": [\"grand bahama\"], \"wikipedia_urls\": [\"https://en.wikipedia.org/wiki/Grand_Bahama\"]
-                INPUT: what character did john noble play in lord of the rings?
-                OUTPUT: \"entities_text\": [\"john noble\", \"lord of the rings\"], \"wikipedia_urls\": [\"https://en.wikipedia.org/wiki/John_Noble\", \"https://en.wikipedia.org/wiki/The_Lord_of_the_Rings:_The_Two_Towers\"]
-                INPUT: what city is the state capital of washington?
-                OUTPUT: \"entities_text\": [\"washington\"], \"wikipedia_urls\": [\"https://en.wikipedia.org/wiki/Washington_(state)\"]
-                INPUT: {input}
-                OUTPUT:'''
+        return '''Extract named entities from the text and provide their Wikipedia URLs according to the following examples.
+Never output any sentences, explanations or reasons other than the value of each key.
+
+examples:
+INPUT: what does the letters eu stand for?
+OUTPUT: "entities_text": [], "wikipedia_urls": []
+
+INPUT: what country is the grand bahama island in?
+OUTPUT: "entities_text": ["grand bahama"], "wikipedia_urls": ["https://en.wikipedia.org/wiki/Grand_Bahama"]
+
+INPUT: what character did john noble play in lord of the rings?
+OUTPUT: "entities_text": ["john noble", "lord of the rings"], "wikipedia_urls": ["https://en.wikipedia.org/wiki/John_Noble", "https://en.wikipedia.org/wiki/The_Lord_of_the_Rings:_The_Two_Towers"]
+
+INPUT: what city is the state capital of washington?
+OUTPUT: "entities_text": ["washington"], "wikipedia_urls": ["https://en.wikipedia.org/wiki/Washington_(state)"]'''
--- pipeline/el_wikipedia.py
+++ pipeline/el_wikipedia.py
@@ -1,6 +1,6 @@
 import replicate
 from dotenv import load_dotenv
-from pipeline.data_reader import get_prompt, read_json_file
+from pipeline.data_reader import get_few_shots, read_dataset_file
 
 load_dotenv()
 
@@ -16,18 +16,23 @@ def ChatCompletion(model, prompt, system_prompt):
     return "".join(output)
 
 
-def write_json_file(output_file_path, data, model, dataset):
+def write_json_file(output_file_path, data, model, system_prompt, dataset):
     with open(output_file_path, "a", encoding="UTF-8") as output_file:
         output_file.write("[")
         data_length = len(data)
         for i, item in enumerate(data, start=1):
-            input_text = item.get("utterance", None)
+            if dataset == "lcquad2":
+                input_text = item.get("question", None)
+            elif dataset == "simpleqs":
+                input_text = item.split("\t")[3]
+            elif dataset == "webqsp":
+                input_text = item.get("utterance", None)
 
             if input_text is not None and input_text != "n/a":  # データセットのEL対象文に空値と'n/a'がある
                 output = ChatCompletion(                        # その場合要素が空値のデータを返す
                     model,
-                    prompt=get_prompt(dataset, input_text),
-                    system_prompt="Extract named entities from the following text and provide their Wikipedia URLs"
+                    prompt=f'''INPUT: {input}\nOUTPUT:''',
+                    system_prompt=system_prompt
                 )
             else:
                 output = '"entities_text": [], "wikipedia_urls": []'
@@ -41,9 +46,14 @@ def write_json_file(output_file_path, data, model, dataset):
 
 
 def entity_wikipedia_url_extractor(model, dataset):
-    # INPUT_FILE_PATH = 'datasets/test_datasets/' + dataset + '_test.json'
+    # INPUT_FILE_PATH = f'datasets/test_datasets/{dataset}_test.json'
     INPUT_FILE_PATH = 'datasets/test_datasets/test.json'
-    OUTPUT_FILE_PATH = 'result/' + dataset + '/' + model + '/wikipedia_url.json'
-    data = read_json_file(INPUT_FILE_PATH)
-    model = "meta/" + model + "-chat"
-    write_json_file(OUTPUT_FILE_PATH, data, model, dataset)
+    OUTPUT_FILE_PATH = f'result/{dataset}/{model}/wikipedia_url.json'
+    data = read_dataset_file(dataset, INPUT_FILE_PATH)
+    model = f"meta/{model}-chat"
+    system_prompt = f'''Extract named entities from the text and provide their Wikipedia URLs according to the following examples.
+Never output any sentences, explanations or reasons other than the value of each key.
+
+examples:
+{get_few_shots(dataset)}'''
+    write_json_file(OUTPUT_FILE_PATH, data, model, system_prompt, dataset)

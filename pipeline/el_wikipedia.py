c43b7614898cd12a2bd94dee8d7113feb3290f7b
Fumiya Mitsuji <a5820079@aoyama.jp>
2024-02-23 16:18:17 +0900

    修正

--- pipeline/el_wikipedia.py
+++ pipeline/el_wikipedia.py
@@ -1,4 +1,5 @@
 import replicate
+import time
 from dotenv import load_dotenv
 from pipeline.data_reader import get_few_shots, read_dataset_file
 
@@ -31,7 +32,7 @@ def write_json_file(output_file_path, data, model, system_prompt, dataset):
             if input_text is not None and input_text != "n/a":  # データセットのEL対象文に空値と'n/a'がある
                 output = ChatCompletion(                        # その場合要素が空値のデータを返す
                     model,
-                    prompt=f'''INPUT: {input}\nOUTPUT:''',
+                    prompt=f'''INPUT: {input_text}\nOUTPUT:''',
                     system_prompt=system_prompt
                 )
             else:
@@ -44,10 +45,12 @@ def write_json_file(output_file_path, data, model, system_prompt, dataset):
             output_file.write("\n")
         output_file.write("]")
 
+        time.sleep(1)
+
 
 def entity_wikipedia_url_extractor(model, dataset):
-    # INPUT_FILE_PATH = f'datasets/test_datasets/{dataset}_test.json'
-    INPUT_FILE_PATH = 'datasets/test_datasets/test.json'
+    INPUT_FILE_PATH = f'datasets/test_datasets/{dataset}_test.json'
+    # INPUT_FILE_PATH = 'datasets/test_datasets/test.json'
     OUTPUT_FILE_PATH = f'result/{dataset}/{model}/wikipedia_url.json'
     data = read_dataset_file(dataset, INPUT_FILE_PATH)
     model = f"meta/{model}-chat"
@@ -56,4 +59,5 @@ Never output any sentences, explanations or reasons other than the value of each
 
 examples:
 {get_few_shots(dataset)}'''
+
     write_json_file(OUTPUT_FILE_PATH, data, model, system_prompt, dataset)

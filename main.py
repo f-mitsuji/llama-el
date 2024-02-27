588c4e783c50526c0c9f69198bc6b85a3ceb870a
Fumiya Mitsuji <a5820079@aoyama.jp>
2024-02-28 02:55:14 +0900

    eval.py実装

--- main.py
+++ main.py
@@ -1,53 +1,50 @@
 import argparse
 
-from pipeline.el_wikipedia import entity_wikipedia_url_extractor
 from pipeline.el_wikidata import wikipedia_url_to_wikidata_id
-from pipeline.eval import calculate_metrics
+from pipeline.el_wikipedia import entity_wikipedia_url_extractor
+from pipeline.eval import evaluate_model_prediction
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description='Arguments of input for task functions'
+    parser = argparse.ArgumentParser(description="Arguments of input for task functions")
+
+    parser.add_argument(
+        "-t",
+        "--task",
+        type=str,
+        # default='url',
+        choices=["url", "id", "eval"],
+        help="Task",
     )
 
     parser.add_argument(
-        '-t',
-        '--task',
+        "-m",
+        "--model",
         type=str,
-        default='url',
-        choices=['url', 'id', 'eval'],
-        help='Task'
+        default="llama-2-70b",
+        choices=["llama-2-7b", "llama-2-13b", "llama-2-70b"],
+        help="Model",
     )
 
     parser.add_argument(
-        '-m',
-        '--model',
+        "-d",
+        "--dataset",
         type=str,
-        default='llama-2-70b',
-        choices=['llama-2-7b', 'llama-2-13b', 'llama-2-70b'],
-        help='Model'
-    )
-
-    parser.add_argument(
-        '-d',
-        '--dataset',
-        type=str,
-        default='webqsp',
-        choices=['laquad2', 'simpleqs', 'webqsp'],
-        help='Dataset'
+        # default='webqsp',
+        choices=["lcquad2", "simpleqs", "webqsp"],
+        help="Dataset",
     )
 
     args = parser.parse_args()
 
-    if args.task == 'url':
+    if args.task == "url":
         entity_wikipedia_url_extractor(args.model, args.dataset)
-    elif args.task == 'id':
+    elif args.task == "id":
         wikipedia_url_to_wikidata_id(args.model, args.dataset)
-    elif args.task == 'eval':
-        calculate_metrics()
+    elif args.task == "eval":
+        evaluate_model_prediction(args.model, args.dataset)
     else:
-        raise NotImplementedError(
-            "Please select your task from ['url', 'id', 'eval'].")
+        raise NotImplementedError("Please select your task from ['url', 'id', 'eval'].")
 
 
 if __name__ == "__main__":
--- pipeline/eval.py
+++ pipeline/eval.py
@@ -1,2 +1,45 @@
-def calculate_metrics():
-    print('eval')
+from pipeline.data_reader import read_correct_wikidata_ids, read_predicted_wikidata_ids
+
+
+def calculate_evaluation_metrics(correct_ids, predicted_ids):
+    metrics = {"precision": [], "recall": [], "f1": []}
+
+    for true_ids, predicted_ids_for_line in zip(correct_ids, predicted_ids):
+        true_positive = len(set(predicted_ids_for_line) & set(true_ids))
+        false_positive = len(set(predicted_ids_for_line) - set(true_ids))
+
+        if not true_ids and not predicted_ids_for_line:
+            true_positive = 1
+            false_positive = 0
+
+        precision = true_positive / (true_positive + false_positive) if (true_positive + false_positive) > 0 else 0
+        recall = true_positive / len(true_ids) if len(true_ids) > 0 else 0
+        f1 = 2 * precision * recall / (precision + recall) if (precision + recall) > 0 else 0
+
+        metrics["precision"].append(precision)
+        metrics["recall"].append(recall)
+        metrics["f1"].append(f1)
+
+    total_lines = len(correct_ids)
+    average_metrics = {key: sum(values) / total_lines for key, values in metrics.items() if total_lines > 0}
+
+    return average_metrics.get("precision", 0), average_metrics.get("recall", 0), average_metrics.get("f1", 0)
+
+
+def evaluate_model_prediction(model, dataset):
+    correct_wikidata_file_path = f"datasets/test_datasets/{dataset}_test.json"
+    predicted_wikidata_file_path = f"result/{dataset}/{model}/wikidata_id.json"
+
+    correct_wikidata_ids = read_correct_wikidata_ids(correct_wikidata_file_path)
+    predicted_wikidata_ids = read_predicted_wikidata_ids(predicted_wikidata_file_path)
+
+    precision, recall, f1 = calculate_evaluation_metrics(correct_wikidata_ids, predicted_wikidata_ids)
+
+    print_results(precision, recall, f1)
+
+
+def print_results(precision, recall, f1):
+    print("\n結果:")
+    print(f"適合率: {precision:.3f}")
+    print(f"再現率: {recall:.3f}")
+    print(f"F値: {f1:.3f}")

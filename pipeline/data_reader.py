b86f9f2930d4e2bb10da3ca73ea333caeebd6164
Fumiya Mitsuji <fumiya.mitsuji@gmail.com>
2024-03-08 20:10:08 +0900

    language arg追加（日本語対応のため）

--- pipeline/data_reader.py
+++ pipeline/data_reader.py
@@ -59,31 +59,70 @@ def read_predicted_wikidata_ids(file_path):
     return predicted_wikidata_ids
 
 
-def get_few_shots(dataset):
-    if dataset == "lcquad2":
-        return """INPUT: What does emigration mean?
+def get_few_shots(dataset, language):
+    responses = {
+        (
+            "english",
+            "lcquad2",
+        ): """INPUT: What does emigration mean?
 OUTPUT: "entities_text": [], "wikipedia_urls": []
 INPUT: Who is the child of Ranavalona I's husband?
 OUTPUT: "entities_text": ["Ranavalona I"], "wikipedia_urls": ["https://en.wikipedia.org/wiki/Ranavalona_I"]
 INPUT: What periodical literature does Delta Air Lines use as a moutpiece?
 OUTPUT: "entities_text": ["periodical literature", "Delta Air Lines"], "wikipedia_urls": ["https://en.wikipedia.org/wiki/Periodical_literature", "https://en.wikipedia.org/wiki/Delta_Air_Lines"]
 INPUT: What is award received of Hans Krebs where point in time is 1966-0-0?
-OUTPUT: "entities_text": ["Hans Krebs"], "wikipedia_urls": ["https://en.wikipedia.org/wiki/Hans_Krebs_(biochemist)"]"""
-
-    elif dataset == "simpleqs":
-        return """INPUT: How do plants grow?
+OUTPUT: "entities_text": ["Hans Krebs"], "wikipedia_urls": ["https://en.wikipedia.org/wiki/Hans_Krebs_(biochemist)"]""",
+        (
+            "english",
+            "simpleqs",
+        ): """INPUT: How do plants grow?
 OUTPUT: "entities_text": [], "wikipedia_urls": []
 INPUT: what movie is produced by warner bros.
 OUTPUT: "entities_text": ["warner bros."], "wikipedia_urls": ["https://en.wikipedia.org/wiki/Warner_Bros."]
 INPUT: what is the gender of james hendry?
-OUTPUT: "entities_text": ["james hendry"], "wikipedia_urls": ["https://en.wikipedia.org/wiki/James_Hendry_(obstetrician)"]"""
-
-    elif dataset == "webqsp":
-        return """INPUT: what does the letters eu stand for?
+OUTPUT: "entities_text": ["james hendry"], "wikipedia_urls": ["https://en.wikipedia.org/wiki/James_Hendry_(obstetrician)"]""",
+        (
+            "english",
+            "webqsp",
+        ): """INPUT: what does the letters eu stand for?
 OUTPUT: "entities_text": [], "wikipedia_urls": []
 INPUT: what country is the grand bahama island in?
 OUTPUT: "entities_text": ["grand bahama"], "wikipedia_urls": ["https://en.wikipedia.org/wiki/Grand_Bahama"]
 INPUT: what character did john noble play in lord of the rings?
 OUTPUT: "entities_text": ["john noble", "lord of the rings"], "wikipedia_urls": ["https://en.wikipedia.org/wiki/John_Noble", "https://en.wikipedia.org/wiki/The_Lord_of_the_Rings:_The_Two_Towers"]
 INPUT: what city is the state capital of washington?
-OUTPUT: "entities_text": ["washington"], "wikipedia_urls": ["https://en.wikipedia.org/wiki/Washington_(state)"]"""
+OUTPUT: "entities_text": ["washington"], "wikipedia_urls": ["https://en.wikipedia.org/wiki/Washington_(state)"]""",
+        (
+            "japanese",
+            "lcquad2",
+        ): """入力: 移民ってどういう意味?
+出力: "entities_text": [], "wikipedia_urls": []
+入力: ガリンスタンの相物質の前提条件は何ですか?
+出力: "entities_text": ["ガリンスタン"], "wikipedia_urls": ["https://ja.wikipedia.org/wiki/ガリンスタン"]
+入力: ハンセン病の治療に必要な必須医薬品は何ですか?
+出力: "entities_text": ["ハンセン病", "必須医薬品"], "wikipedia_urls": ["https://ja.wikipedia.org/wiki/ハンセン病", "https://ja.wikipedia.org/wiki/必須医薬品"]
+入力: ポルックスの地形的分離が 0.7 に等しいというのは本当ですか?
+出力: "entities_text": ["ポルックス"], "wikipedia_urls": ["https://ja.wikipedia.org/wiki/ポッルチェ"]""",
+        (
+            "japanese",
+            "simpleqs",
+        ): """入力: 植物はどのように成長しますか?
+出力: "entities_text": [], "wikipedia_urls": []
+入力: ワーナー・ブラザースが製作する映画.
+出力: "entities_text": ["ワーナー・ブラザース"], "wikipedia_urls": ["https://ja.wikipedia.org/wiki/ワーナー・ブラザース"]
+入力: ヒューロン郡にある都市は何ですか
+出力: "entities_text": ["ヒューロン郡"], "wikipedia_urls": ["https://ja.wikipedia.org/wiki/ヒューロン郡_(オハイオ州)"]""",
+        (
+            "japanese",
+            "webqsp",
+        ): """入力: euの文字は何の略ですか?
+出力: "entities_text": [], "wikipedia_urls": []
+入力: グランドバハマ島はどこの国?
+出力: "entities_text": ["グランドバハマ島"], "wikipedia_urls": ["https://ja.wikipedia.org/wiki/グランド・バハマ島"]
+入力: ロード・オブ・ザ・リングでジョン・ノーブルが演じた役は?
+出力: "entities_text": ["ロード・オブ・ザ・リング", "ジョン・ノーブル"], "wikipedia_urls": ["https://ja.wikipedia.org/wiki/ロード・オブ・ザ・リング/二つの塔", "https://ja.wikipedia.org/wiki/ジョン・ノーブル"]
+入力: ワシントン州の州都は何市?
+出力: "entities_text": ["ワシントン州"], "wikipedia_urls": ["https://ja.wikipedia.org/wiki/ワシントン州"]""",
+    }
+
+    return responses.get((language, dataset), "No data available for the given language and dataset combination.")
--- pipeline/el_wikipedia.py
+++ pipeline/el_wikipedia.py
@@ -1,41 +1,107 @@
 import time
 
 import replicate
+import torch
 from dotenv import load_dotenv
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 from pipeline.data_reader import get_few_shots, read_dataset_file
 
 load_dotenv()
 
 
-def ChatCompletion(model: str, prompt: str, system_prompt: str) -> str:
+def ChatCompletion_replicate(model: str, prompt: str, system_prompt: str) -> str:
     output = replicate.run(model, input={"system_prompt": system_prompt, "prompt": prompt, "temperature": 0.01})
     return "".join(output)
 
 
+def ChatCompletion_swallow(system_prompt, input_text, model, tokenizer):
+    prompt_template = "{system_prompt}" "入力:{input}" "出力:"
+    prompt = prompt_template.format(system_prompt=system_prompt, input=input_text)
+
+    input_ids = tokenizer.encode(prompt, add_special_tokens=False, return_tensors="pt")
+    tokens = model.generate(
+        input_ids.to(model.device),
+        max_new_tokens=128,
+        temperature=0.01,
+        top_p=0.95,
+        do_sample=True,
+    )
+    generated_text = tokenizer.decode(tokens[0], skip_special_tokens=True)
+
+    # 生成されたテキストからプロンプト部分を削除
+    generated_response = generated_text[len(prompt) :].strip()
+
+    return generated_response
+
+
+# def ChatCompletion_swallow(instruction, input_text, model, tokenizer):
+#     prompt_template = (
+#         "以下に、あるタスクを説明する指示があり、それに付随する入力が更なる文脈を提供しています。"
+#         "リクエストを適切に完了するための回答を記述してください。\n\n"
+#         "### 指示:\n{instruction}\n\n### 入力:\n{input}\n\n### 応答:"
+#     )
+#     prompt = prompt_template.format(instruction=instruction, input=input_text)
+
+#     input_ids = tokenizer.encode(prompt, add_special_tokens=False, return_tensors="pt")
+#     tokens = model.generate(
+#         input_ids.to(model.device),
+#         max_new_tokens=128,
+#         temperature=0.01,
+#         top_p=0.95,
+#         do_sample=True,
+#     )
+
+#     return tokenizer.decode(tokens[0], skip_special_tokens=True)
+
+
+def extract_input_text(item, dataset, language):
+    if language == "japanese":
+        return item.get("question", None)
+    if dataset == "lcquad2":
+        return item.get("question", None)
+    elif dataset == "simpleqs":
+        return item.split("\t")[3]
+    elif dataset == "webqsp":
+        return item.get("utterance", None)
+    return None
+
+
+def get_output(input_text, language, model_name, system_prompt):
+    if input_text is None or input_text == "n/a":
+        return '{"entities_text": [], "wikipedia_urls": []}'
+
+    if language == "english":
+        output = ChatCompletion_replicate(
+            model_name, prompt=f"""INPUT: {input_text}\nOUTPUT:""", system_prompt=system_prompt
+        )
+        time.sleep(1)  # 最低0.1 2024/2/23
+        return output
+    elif language == "japanese":
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        model = AutoModelForCausalLM.from_pretrained(
+            model_name, torch_dtype=torch.bfloat16, low_cpu_mem_usage=True, device_map="auto"
+        )
+        return ChatCompletion_swallow(system_prompt, input_text, model, tokenizer)
+
+
 # dataの型ヒントは扱うデータのみ
 def write_json_file(
-    output_file_path: str, data: list[dict[str, str]] | list[str], model: str, system_prompt: str, dataset: str
+    output_file_path: str,
+    data: list[dict[str, str]] | list[str],
+    model_name: str,
+    system_prompt: str,
+    dataset: str,
+    language: str,
 ) -> None:
+
     with open(output_file_path, "a", encoding="UTF-8") as output_file:
         output_file.write("[")
         data_length = len(data)
 
         for i, item in enumerate(data, start=1):
-            if dataset == "lcquad2":
-                input_text = item.get("question", None)  # type:ignore
-            elif dataset == "simpleqs":
-                input_text = item.split("\t")[3]  # type:ignore
-            elif dataset == "webqsp":
-                input_text = item.get("utterance", None)  # type:ignore
-
-            # データセットのEL対象文に空値と'n/a'がある．その場合要素が空値のデータを返す
-            if input_text is not None and input_text != "n/a":
-                output = ChatCompletion(model, prompt=f"""INPUT: {input_text}\nOUTPUT:""", system_prompt=system_prompt)
-            else:
-                output = '{"entities_text": [], "wikipedia_urls": []}'
-
-            # output_file.write(f'{{"index": {i}, {output}}}')
+            input_text = extract_input_text(item, dataset, language)
+            output = get_output(input_text, language, model_name, system_prompt)
             output_file.write(output)
 
             if i < data_length:
@@ -43,21 +109,46 @@ def write_json_file(
             output_file.write("\n")
         output_file.write("]")
 
-        time.sleep(1)  # 最低0.1 2024/2/23
 
-
-def entity_wikipedia_url_extractor(model: str, dataset: str) -> None:
-    file_extension = ".txt" if dataset == "simpleqs" else ".json"
-    INPUT_FILE_PATH = f"datasets/test_datasets/{dataset}_test{file_extension}"
-    OUTPUT_FILE_PATH = f"result/{dataset}/{model}/wikipedia_url.json"
-    # INPUT_FILE_PATH = "datasets/test_datasets/test.txt"
-    # OUTPUT_FILE_PATH = f"result/{dataset}/{model}/wikipedia_url_test.json"
-    data = read_dataset_file(dataset, INPUT_FILE_PATH)
-    model = f"meta/{model}-chat"
-    system_prompt = f""""Extract named entities from the text and provide their Wikipedia URLs, as in the examples below.
+def get_config(language: str, dataset: str, model: str) -> dict:
+    configs = {
+        "english": {
+            "file_extension": ".txt" if dataset == "simpleqs" else ".json",
+            "model_prefix": "meta",
+            "model_suffix": "-chat",
+            "system_prompt": """Extract named entities from the text and provide their Wikipedia URLs, as in the examples below.
 Do not output any text other than the keys and values in JSON.
 
 examples:
-{get_few_shots(dataset)}"""
+""",
+        },
+        "japanese": {
+            "file_extension": ".json",
+            "model_prefix": "tokyotech-llm",
+            "model_suffix": "-instruct-hf",
+            "system_prompt": """以下の例のように、テキストから固有表現を抽出し、それらのWikipediaのURLを示せ。
+JSONのキーと値以外のテキストは出力しないでください。
 
-    write_json_file(OUTPUT_FILE_PATH, data[8945:], model, system_prompt, dataset)
+例:
+""",
+        },
+    }
+
+    if language not in configs:
+        raise ValueError("Unsupported language.")
+
+    config = configs[language]
+    config["model"] = f"{config['model_prefix']}/{model}{config['model_suffix']}"
+    config["system_prompt"] += get_few_shots(dataset, language)
+    return config
+
+
+def entity_wikipedia_url_extractor(model: str, dataset: str, language: str) -> None:
+    config = get_config(language, dataset, model)
+    input_file_path = "datasets/test_datasets/test.json"
+    output_file_path = f"result/{dataset}/{model}/test.json"
+    # input_file_path = f"datasets/test_datasets/{dataset}_test{config['file_extension']}"
+    # output_file_path = f"result/{dataset}/{model}/wikipedia_url.json"
+
+    data = read_dataset_file(dataset, input_file_path)
+    write_json_file(output_file_path, data, config["model"], config["system_prompt"], dataset, language)

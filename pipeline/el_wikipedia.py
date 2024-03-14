17da5e2784e68f11b6da322dfb7c21a0b3336dc9
Fumiya Mitsuji <fumiya.mitsuji@gmail.com>
2024-03-14 21:53:16 +0900

    feat: swallowに基づく日本語EL機能の実装

--- pipeline/el_wikipedia.py
+++ pipeline/el_wikipedia.py
@@ -1,3 +1,4 @@
+import re
 import time
 
 import replicate
@@ -15,44 +16,28 @@ def ChatCompletion_replicate(model: str, prompt: str, system_prompt: str) -> str
     return "".join(output)
 
 
-def ChatCompletion_swallow(system_prompt, input_text, model, tokenizer):
-    prompt_template = "{system_prompt}" "入力:{input}" "出力:"
-    prompt = prompt_template.format(system_prompt=system_prompt, input=input_text)
+def ChatCompletion_swallow(instruction, input_text, model, tokenizer):
+    prompt_template = (
+        "以下に、あるタスクを説明する指示があり、それに付随する入力が更なる文脈を提供しています。"
+        "リクエストを適切に完了するための回答を記述してください。\n\n"
+        "### 指示:\n{instruction}\n\n入力: {input}\n出力:"
+    )
+    prompt = prompt_template.format(instruction=instruction, input=input_text)
 
     input_ids = tokenizer.encode(prompt, add_special_tokens=False, return_tensors="pt")
     tokens = model.generate(
         input_ids.to(model.device),
         max_new_tokens=128,
         temperature=0.01,
-        top_p=0.95,
+        # top_p=0.95,
         do_sample=True,
     )
+
     generated_text = tokenizer.decode(tokens[0], skip_special_tokens=True)
 
-    # 生成されたテキストからプロンプト部分を削除
     generated_response = generated_text[len(prompt) :].strip()
 
     return generated_response
-
-
-# def ChatCompletion_swallow(instruction, input_text, model, tokenizer):
-#     prompt_template = (
-#         "以下に、あるタスクを説明する指示があり、それに付随する入力が更なる文脈を提供しています。"
-#         "リクエストを適切に完了するための回答を記述してください。\n\n"
-#         "### 指示:\n{instruction}\n\n### 入力:\n{input}\n\n### 応答:"
-#     )
-#     prompt = prompt_template.format(instruction=instruction, input=input_text)
-
-#     input_ids = tokenizer.encode(prompt, add_special_tokens=False, return_tensors="pt")
-#     tokens = model.generate(
-#         input_ids.to(model.device),
-#         max_new_tokens=128,
-#         temperature=0.01,
-#         top_p=0.95,
-#         do_sample=True,
-#     )
-
-#     return tokenizer.decode(tokens[0], skip_special_tokens=True)
 
 
 def extract_input_text(item, dataset, language):
@@ -85,7 +70,6 @@ def get_output(input_text, language, model_name, system_prompt):
         return ChatCompletion_swallow(system_prompt, input_text, model, tokenizer)
 
 
-# dataの型ヒントは扱うデータのみ
 def write_json_file(
     output_file_path: str,
     data: list[dict[str, str]] | list[str],
@@ -99,15 +83,27 @@ def write_json_file(
         output_file.write("[")
         data_length = len(data)
 
+        not_matching_count = 0
+
         for i, item in enumerate(data, start=1):
             input_text = extract_input_text(item, dataset, language)
             output = get_output(input_text, language, model_name, system_prompt)
-            output_file.write(output)
+
+            if not re.search(r'^"entities_text": \[.*\],\s*"wikipedia_urls": \[.*\]$', output, re.S):
+                not_matching_count += 1
+                print(f"Index not matching: {i}")
+
+            if language == "japanese":
+                output_file.write(f'{{"id": {i}, {output}}}')
+            else:
+                output_file.write(output)
 
             if i < data_length:
                 output_file.write(",")
             output_file.write("\n")
         output_file.write("]")
+
+        print(f"Total not matching: {not_matching_count}")
 
 
 def get_config(language: str, dataset: str, model: str) -> dict:
@@ -116,6 +112,7 @@ def get_config(language: str, dataset: str, model: str) -> dict:
             "file_extension": ".txt" if dataset == "simpleqs" else ".json",
             "model_prefix": "meta",
             "model_suffix": "-chat",
+            "language_suffix": "",
             "system_prompt": """Extract named entities from the text and provide their Wikipedia URLs, as in the examples below.
 Do not output any text other than the keys and values in JSON.
 
@@ -126,9 +123,9 @@ examples:
             "file_extension": ".json",
             "model_prefix": "tokyotech-llm",
             "model_suffix": "-instruct-hf",
+            "language_suffix": "_japanese",
             "system_prompt": """以下の例のように、テキストから固有表現を抽出し、それらのWikipediaのURLを示せ。
-JSONのキーと値以外のテキストは出力しないでください。
-
+JSONのキーと値以外のテキストは出力しないでください。与えられた入力にのみ答えてください。
 例:
 """,
         },
@@ -145,10 +142,9 @@ JSONのキーと値以外のテキストは出力しないでください。
 
 def entity_wikipedia_url_extractor(model: str, dataset: str, language: str) -> None:
     config = get_config(language, dataset, model)
-    input_file_path = "datasets/test_datasets/test.json"
-    output_file_path = f"result/{dataset}/{model}/test.json"
-    # input_file_path = f"datasets/test_datasets/{dataset}_test{config['file_extension']}"
-    # output_file_path = f"result/{dataset}/{model}/wikipedia_url.json"
-
-    data = read_dataset_file(dataset, input_file_path)
+    # input_file_path = "datasets/test_datasets/test.json"
+    # output_file_path = f"result/{dataset}/{model}/test.json"
+    input_file_path = f"datasets/test_datasets/{dataset}{config['language_suffix']}_test{config['file_extension']}"
+    output_file_path = f"result/{dataset}/{model}/wikipedia_url.json"
+    data = read_dataset_file(input_file_path)
     write_json_file(output_file_path, data, config["model"], config["system_prompt"], dataset, language)
